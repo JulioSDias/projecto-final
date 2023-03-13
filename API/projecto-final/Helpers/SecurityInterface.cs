655ff81aed1e1dcf7f0c1fe0ffd29ba9fe2f4f36
unknown <j.simoes.dias@gmail.com>
2023-03-13 15:05:08 +0000

    fixed extra files

--- API/projecto-final/Helpers/SecurityInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SodokuAPI.Helpers
-{
-    public interface ISecurity
-    {
-        Byte[] CreatePasswordSalt(string password);
-        Byte[] CreatePasswordHash(string password, byte[] salt);
-        Boolean VerifyPassword(string password, byte[] storedHash, byte[] storedSalt);
-    }
-}
