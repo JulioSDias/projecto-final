1c1c78175c36d7b31f06511378d6cad8da82a68d
unknown <j.simoes.dias@gmail.com>
2023-04-05 01:57:25 +0100

    add product

--- FrontEnd/src/app/Control Panel/products.component.ts
+++ FrontEnd/src/app/Control Panel/products.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Product } from "../Shared/Entities/product";
 import { ControlPanelService } from "../Shared/Services/controlPanel.service";
@@ -8,7 +8,11 @@ import { ControlPanelService } from "../Shared/Services/controlPanel.service";
     templateUrl: "products.component.html"
 })
 
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+    addProductForm!: FormGroup;
+    imageUrl!: string;
+    loading = false;
+    submitted = false;
     products?: Array<Product>;
     openAccordion: Array<boolean> = [];
     isEditable: Array<boolean> = [];
@@ -19,12 +23,76 @@ export class ProductsComponent {
     ) { 
         this.getTable();
     }
+    
+    get values() {
+        return this.addProductForm.controls;
+    }
 
+    get genresArray(): FormArray {
+        return this.addProductForm.get('genresArray') as FormArray;
+    }
+    
+    get imageControls() {
+      return this.addProductForm.get('images') as FormArray;
+    }
+    
+    ngOnInit(){
+      this.addProductForm = this.formBuilder.group({
+        name: [null, Validators.required],
+        price: [null, Validators.required],
+        stock: [null, Validators.required],
+        console: [null, Validators.required],
+        description: [null, Validators.required],
+        genresArray: this.formBuilder.array(['']),
+        images: this.formBuilder.array([]),
+      });
+      this.addImage();
+    }
+
+    
+    addGenre() {
+      this.genresArray.push(this.formBuilder.control(''));
+    }
+
+    removeGenre(i: number) {
+      this.genresArray.removeAt(i);
+    }
+
+    addImage() {
+      const imageGroup = this.formBuilder.group({
+        imageFile: [''],
+        imageUrl: [''],
+      });
+      this.imageControls.push(imageGroup);
+    }
+    
+    onFileSelected(event: any, index: number) {
+      const file = event.target.files[0];
+      if (file) {
+        this.imageControls.at(index).get('imageFile')!.setValue(file);
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imageControls.at(index).get('imageUrl')!.setValue(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+
+    getImageUrl(imageControl: AbstractControl) {
+      const imageUrl = imageControl.get('imageUrl')!.value;
+      return imageUrl ? imageUrl : null;
+    }
+    
+    removeImage(i: number){
+      this.imageControls.removeAt(i);
+    }
+    
     getTable() {
         this.controlPanelService.getAllProducts()
             .subscribe({
                 next: (data : Product[]) => {
                     this.products = data;
+                    console.log(data);
                 }
             });
     }
@@ -37,4 +105,12 @@ export class ProductsComponent {
                 }
             });
     }
+
+    addNewProduct() {
+        const newProduct = new FormData();
+
+        newProduct.append("name", this.addProductForm.get("name")?.value); 
+        console.log(this.addProductForm.value); 
+    }
  }
+
